77bf93f66d98a0fcd4a55ab5b6850ad5f92dabd9
thanhle1547 <thanhlm72@wru.vn>
2022-09-29 22:12:24 +0700

    Update log_interceptor.dart

--- lib/data/interceptors/log_interceptor.dart
+++ lib/data/interceptors/log_interceptor.dart
@@ -4,16 +4,6 @@ import 'package:flutter_simple_base_project/utils/helpers/system_utils.dart';
 const _source = 'LogInterceptor';
 
 class LogInterceptor extends InterceptorsWrapper {
-  static List<Map> sHttpResponses = [];
-  static List<String> sResponsesHttpUrl = [];
-
-  static List<Map<String, dynamic>> sHttpRequest = [];
-  static List<String> sRequestHttpUrl = [];
-
-  static List<Map<String, dynamic>> sHttpError = [];
-
-  static List<String> sHttpErrorUrl = [];
-
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
     super.onRequest(options, handler);
@@ -27,22 +17,6 @@ class LogInterceptor extends InterceptorsWrapper {
       body/query: ${options.data}
     ''',
     );
-
-    try {
-      addLogic(sRequestHttpUrl, options.path);
-
-      var data = options.data ?? {};
-
-      var map = {
-        "header": {...options.headers},
-      };
-
-      if (options.method == "POST") map["data"] = options.data;
-
-      addLogic(sHttpRequest, data);
-    } catch (e) {
-      SystemUtils.debugLog(_source, e.toString());
-    }
   }
 
   @override
@@ -52,60 +26,23 @@ class LogInterceptor extends InterceptorsWrapper {
     SystemUtils.debugLog(
       _source,
       '''
-      Error: ${err.toString()}
-      Error response: ${err.response?.toString()}
+      Error: $err
+      Error response: ${err.response}
+      URL: ${err.requestOptions.path}
     ''',
     );
-
-    try {
-      addLogic(sHttpErrorUrl, err.requestOptions.path);
-      var errors = {};
-      errors["error"] = err.message;
-      addLogic(sHttpError, errors);
-    } catch (e) {
-      SystemUtils.debugLog(_source, e.toString());
-    }
   }
 
   @override
   void onResponse(Response response, ResponseInterceptorHandler handler) {
     super.onResponse(response, handler);
+
     SystemUtils.debugLog(
       _source,
       '''
       Response: ${response.requestOptions.path}
-      ${response.toString()}
+      $response
     ''',
     );
-
-    // ignore: prefer_typing_uninitialized_variables
-    late var data;
-    try {
-      if (response.data is Map ||
-          response.data is List ||
-          response.data is String) {
-        data = {};
-        data["data"] = response.data;
-      } else if (response.data != null) {
-        data = response.data.toRecruiterJson(); // type String
-      }
-    } catch (e) {
-      SystemUtils.debugLog(_source, e.toString());
-    }
-
-    addLogic(
-      sResponsesHttpUrl,
-      response.requestOptions.uri.toString(),
-    );
-    addLogic(sHttpResponses, data);
-  }
-
-  static addLogic(List list, data) {
-    if (list.length > 20) {
-      SystemUtils.debugLog(_source, list.toString());
-      list.removeAt(0);
-    }
-
-    list.add(data);
   }
 }
